ea5d54265972f4714a176b806a48451d7406c8a7
Logan1allen <logan.allen@bsu.edu>
2024-11-08 10:29:12 -0500

    Coin
    Adds the coin(strawberry milk) and has it falling at random speeds and positions

--- LoganAllen_BasicGame.py
+++ LoganAllen_BasicGame.py
@@ -14,14 +14,32 @@ class beardMan(simpleGE.Sprite):
             self.x -= self.moveSpeed
         if self.isKeyPressed(pygame.K_RIGHT):
             self.x += self.moveSpeed
-#class Coin(simpleGE.Sprite):
+class Coin(simpleGE.Sprite):
+    def __init__(self, scene):
+        super().__init__(scene)
+        self.setImage("strawberry.png")
+        self.setSize(50,50)
+        self.minSpeed = 3
+        self.maxSpeed = 8
+        self.reset()
+        
+    def reset(self):
+        self.y = 10
+        self.x = random.randint(0, self.screenWidth)
+        self.dy = random.randint(self.minSpeed, self.maxSpeed)
+        
+    def checkBounds(self):
+        if self.bottom > self.screenHeight:
+            self.reset()
               
 class Game(simpleGE.Scene):
     def __init__(self):
         super().__init__()
         self.setImage("frogBaby.png")
         self.beardMan = beardMan(self)
-        self.sprites = [self.beardMan]
+        self.strawberry = Coin(self)
+        self.sprites = [self.beardMan,
+                        self.strawberry]
         
         
 def main():
